849bce411ad36ebb2432ba67604a9ef733a52792
nicemona <thassamonp@gmail.com>
2022-07-10 15:32:15 +0700

    feat: mock data

---
+++ database/mockDB.js
@@ -0,0 +1,12 @@
+export const users = [
+  {
+    name: 'John',
+    email: 'john@example.com',
+    projects: [{ title: "John's project" }, { title: "John's summer project" }],
+  },
+  {
+    name: 'Jane',
+    email: 'jane@example.com',
+    projects: [{ title: "Jane's project" }],
+  },
+];
